eb86294133ad22c2c3c1910cda6d05b62cb28973
Hana <hanabia2014@gmail.com>
2025-03-18 20:04:41 -0500

    Database schema design and User Authentication created

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,35 @@
- 
+const express = require('express')
+const app = express();
+const port = 5500
+
+
+// db Connection 
+const dbConnection = require('./db/dbConfig');
+
+// User Routes middleware file
+const userRouter = require("./routes/userRoute");
+
+//json middleware to extract json data
+app.use(express.json())
+
+// User Routes middleware
+app.use("/api/users", userRouter )
+
+// Question Routes middleware ??
+
+// Answer Routes middleware ??
+
+async function start (){
+    try {
+        const result = await dbConnection.execute("select 'test' ")
+        app.listen(port)
+        console.log("database connection established")
+        console.log(`listening on ${port}`)
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+start()
+
+
+

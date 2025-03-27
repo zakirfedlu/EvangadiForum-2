39569946b96eca3550c882b16c6a897ebcbdb4f4
Henok Binayew <henokbinayew21@gmail.com>
2025-03-27 17:38:04 +0300

    dark-mode

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,100 +1,31 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import { FiLogOut, FiMenu, FiX } from 'react-icons/fi';
-import style from './Header.module.css';
-import logo from '../../assets/images/HeaderLogo.png';
-import { Link, useNavigate } from 'react-router-dom';
-
-const Header = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const navigate = useNavigate();
-=======
-import React, { useState, useEffect } from "react";
-import { FiMenu, FiX } from "react-icons/fi"; // For hamburger and close icons
+import React, { useState, useEffect, useContext } from "react";
+import { FiLogOut, FiMenu, FiX, FiSun, FiMoon } from "react-icons/fi"; // Icons for menu and logout
 import style from "./Header.module.css";
 import logo from "../../assets/images/HeaderLogo.png";
 import { Link, useNavigate } from "react-router-dom";
+import { ThemeContext } from "../../ThemeContext";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Track login state
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
->>>>>>> 879f195f65fb2b7f3b6b6e18b1abd52cc9f0bca9
+  const { theme, toggleTheme } = useContext(ThemeContext);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  //!!! Check if the user is logged in on component mount
+  // Check if the user is logged in on component mount
   useEffect(() => {
     const token = localStorage.getItem("token");
-    if (token) {
-      setIsLoggedIn(true); // User is logged in
-    } else {
-      setIsLoggedIn(false); // User is not logged in
-    }
+    setIsLoggedIn(!!token);
   }, []);
 
-<<<<<<< HEAD
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            setIsLoggedIn(true);
-        } else {
-            setIsLoggedIn(false);
-        }
-    }, []);
-=======
->>>>>>> 879f195f65fb2b7f3b6b6e18b1abd52cc9f0bca9
-
-    const handleLogout = () => {
-<<<<<<< HEAD
-        localStorage.removeItem('token');
-        console.log('Token removed:', localStorage.getItem('token'));
-        setIsLoggedIn(false);
-        navigate('/login');
-=======
-        localStorage.removeItem('token'); // Remove token from localStorage
-        console.log('Token removed:', localStorage.getItem('token')); // Check if token is removed
-        setIsLoggedIn(false); 
-        navigate('/login'); // Redirect to login page
->>>>>>> 879f195f65fb2b7f3b6b6e18b1abd52cc9f0bca9
-    };
-
- 
-
-<<<<<<< HEAD
-            <div className={style.hamburger} onClick={toggleMenu}>
-                {isMenuOpen ? <FiX size={30} /> : <FiMenu size={30} />}
-            </div>
-            <div className={`${style.header__nav} ${isMenuOpen ? style.active : ''}`}>
-                <ul className={style.header__nav__list}>
-                    <li>
-                        <Link to="/home" onClick={() => setIsMenuOpen(false)}>Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/howItWorks" onClick={() => setIsMenuOpen(false)}>How it Works</Link>
-                    </li>
-                    <li>
-                        {isLoggedIn ? (
-                            <Link to="/login" onClick={handleLogout} className={style.logOut}>
-                                <button id='log-out'>
-                                    <FiLogOut size={20} className={style.icon} />
-                                    Log Out
-                                </button>
-                            </Link>
-                        ) : (
-                            <Link to="/login" onClick={() => setIsMenuOpen(false)} className={style.signIn}>
-                                <button id='sign-in'>Sign In</button>
-                            </Link>
-                        )}
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
-=======
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+    navigate("/login");
+  };
 
   return (
     <div className={style.header__container}>
@@ -104,40 +35,45 @@ const Header = () => {
         </Link>
       </div>
 
-
-           
-            <div className={style.hamburger} onClick={toggleMenu}>
-                {isMenuOpen ? <FiX size={30} /> : <FiMenu size={30} />}
-            </div>
-
-    
+      <div className={style.hamburger} onClick={toggleMenu}>
+        {isMenuOpen ? <FiX size={30} /> : <FiMenu size={30} />}
+      </div>
 
       <div className={`${style.header__nav} ${isMenuOpen ? style.active : ""}`}>
         <ul className={style.header__nav__list}>
           <li>
-            <Link to="/" onClick={() => setIsMenuOpen(false)}>
+            <Link to="/home" onClick={() => setIsMenuOpen(false)}>
               Home
             </Link>
           </li>
           <li>
-            <Link to="/" onClick={() => setIsMenuOpen(false)}>
+            <Link to="/howItWorks" onClick={() => setIsMenuOpen(false)}>
               How it Works
             </Link>
           </li>
           <li>
             {isLoggedIn ? (
-              <button onClick={handleLogout}>Log Out</button>
+              <button onClick={handleLogout} className={style.logOut}>
+                <div>
+                  <FiLogOut size={20} className={style.icon} /> Log Out
+                </div>
+              </button>
             ) : (
               <Link to="/login" onClick={() => setIsMenuOpen(false)}>
-                <button>Sign In</button>
+                <button className={style.signIn}>Sign In</button>
               </Link>
             )}
+          </li>
+          <li>
+            <button onClick={toggleTheme} className={style.themeToggle}>
+              {/*Implemented different i cons for different theme*/}
+              {theme === "light" ? <FiMoon size={20} /> : <FiSun size={20} />}
+            </button>
           </li>
         </ul>
       </div>
     </div>
   );
->>>>>>> 879f195f65fb2b7f3b6b6e18b1abd52cc9f0bca9
 };
 
 export default Header;
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,13 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import ThemeProvider from "./ThemeContext";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GoogleOAuthProvider clientId="1059491385080-dbgkekb59g3dcjflmue4g21pn9nj973n.apps.googleusercontent.com">
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </GoogleOAuthProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);

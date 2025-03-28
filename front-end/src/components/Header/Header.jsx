9ce4b65a0fbdc3b09f365f8a1835861568f0dfb6
samuel habtamu <152323187+Samuelgit3919@users.noreply.github.com>
2025-03-28 04:01:26 +0300

    commit

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -1,73 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import { FiLogOut, FiMenu, FiX } from 'react-icons/fi';
-import style from './Header.module.css';
-import logo from '../../assets/images/HeaderLogo.png';
-import { Link, useNavigate } from 'react-router-dom';
-
-const Header = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const navigate = useNavigate();
-
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
-
-    useEffect(() => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            setIsLoggedIn(true);
-        } else {
-            setIsLoggedIn(false);
-        }
-    }, []);
-
-    const handleLogout = () => {
-        localStorage.removeItem('token');
-        console.log('Token removed:', localStorage.getItem('token'));
-        setIsLoggedIn(false);
-        navigate('/login');
-    };
-
-    return (
-        <div className={style.header__container}>
-            <div className={style.header__logo}>
-                <Link to="/">
-                    <img src={logo} alt="Evangadi Logo" />
-                </Link>
-            </div>
-
-            <div className={style.hamburger} onClick={toggleMenu}>
-                {isMenuOpen ? <FiX size={30} /> : <FiMenu size={30} />}
-            </div>
-            <div className={`${style.header__nav} ${isMenuOpen ? style.active : ''}`}>
-                <ul className={style.header__nav__list}>
-                    <li>
-                        <Link to="/home" onClick={() => setIsMenuOpen(false)}>Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/howItWorks" onClick={() => setIsMenuOpen(false)}>How it Works</Link>
-                    </li>
-                    <li>
-                        {isLoggedIn ? (
-                            <Link to="/login" onClick={handleLogout} className={style.logOut}>
-                                <button id='log-out'>
-                                    <FiLogOut size={20} className={style.icon} />
-                                    Log Out
-                                </button>
-                            </Link>
-                        ) : (
-                            <Link to="/login" onClick={() => setIsMenuOpen(false)} className={style.signIn}>
-                                <button id='sign-in'>Sign In</button>
-                            </Link>
-                        )}
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
-=======
 import React, { useState, useEffect, useContext } from "react";
 import { FiLogOut, FiMenu, FiX, FiSun, FiMoon } from "react-icons/fi"; // Icons for menu and logout
 import style from "./Header.module.css";
@@ -87,7 +17,6 @@ const Header = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  // Check if the user is logged in on component mount
   useEffect(() => {
     const token = localStorage.getItem("token");
     setIsLoggedIn(!!token);
@@ -146,7 +75,6 @@ const Header = () => {
       </div>
     </div>
   );
->>>>>>> c928fdb44ae62c75d04d82f0dd8b0a20bb71c560
 };
 
 export default Header;
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -5,21 +5,12 @@ import App from "./App.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import ThemeProvider from "./ThemeContext";
 
-<<<<<<< HEAD
-// 319604150752-vpfc05fm7svkcgdfrkufqgusjamg75k5.apps.googleusercontent.com
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <GoogleOAuthProvider clientId="319604150752-vpfc05fm7svkcgdfrkufqgusjamg75k5.apps.googleusercontent.com">
-      <App />
-=======
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId="1059491385080-dbgkekb59g3dcjflmue4g21pn9nj973n.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="319604150752-vpfc05fm7svkcgdfrkufqgusjamg75k5.apps.googleusercontent.com">
       <ThemeProvider>
         <App />
       </ThemeProvider>
->>>>>>> c928fdb44ae62c75d04d82f0dd8b0a20bb71c560
     </GoogleOAuthProvider>
   </StrictMode>
 );

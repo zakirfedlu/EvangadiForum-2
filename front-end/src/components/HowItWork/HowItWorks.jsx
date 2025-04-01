8478bd4953b0686083865816e42b40600290ada5
samuel habtamu <152323187+Samuelgit3919@users.noreply.github.com>
2025-04-01 09:34:45 +0300

    commit

--- front-end/src/components/HowItWork/HowItWorks.jsx
+++ front-end/src/components/HowItWork/HowItWorks.jsx
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import styles from "./howItWorks.module.css";
 import Layout from "../../Layout";
 import axios from "axios";
+import { ScaleLoader } from "react-spinners";
 
 const HowToWork = () => {
     const [data, setData] = useState([])
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         axios.get("/db.json").then((res) => {
             setData(res.data)
+            setLoading(false)
         })
     }, [])
 
@@ -21,15 +24,22 @@ const HowToWork = () => {
                 <h1 className={styles.heading}>How It Works</h1>
                 <p className={styles.introduction}>{introduction}</p>
                 <div className={styles.steps}>
-                    {data.map((item) => (
-                        <div key={item.step} className={styles.stepItem}>
-                            <div className={styles.stepNumber}>{item.step}</div>
-                            <div className={styles.stepContent}>
-                                <h2 className={styles.stepTitle}>{item.title}</h2>
-                                <p className={styles.stepDescription}>{item.description}</p>
+                    {
+
+                        loading ? (
+                            <div className={styles.loading}>
+                                <ScaleLoader />
                             </div>
-                        </div>
-                    ))}
+                        ) : (
+                            data.map((item) => (
+                                <div key={item.step} className={styles.stepItem}>
+                                    <div className={styles.stepNumber}>{item.step}</div>
+                                    <div className={styles.stepContent}>
+                                        <h2 className={styles.stepTitle}>{item.title}</h2>
+                                        <p className={styles.stepDescription}>{item.description}</p>
+                                    </div>
+                                </div>
+                            )))}
                 </div>
             </div>
         </Layout>
